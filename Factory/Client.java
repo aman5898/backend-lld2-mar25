0ee5abc1df7c94e35040844e9da16440396881a3
aman <amankrsingh.2012@gmail.com>
2025-03-17 23:12:19 +0530

    Factory Method Pattern

--- Factory/Client.java
+++ Factory/Client.java
@@ -1,9 +1,21 @@
 package Factory;
 
 
+import Factory.components.Button.Button;
+import Factory.components.Dropdown.Dropdown;
+import Factory.components.Menu.Menu;
+
 public class Client {
     public static void main(String[] args) {
 
+//        Platform p = new Android(); // this is a simulation
 
+        String str = "Android"; // This is a simulation
+        Platform p = Platform.getPlatform(str);
+
+        UIComponentFactory uiComponentFactory = p.createUIComponentFactory();
+        Button b = uiComponentFactory.createButton();
+        Dropdown d = uiComponentFactory.createDropdown();
+        Menu menu = uiComponentFactory.createMenu();
     }
 }
